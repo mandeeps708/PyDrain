0b0cac91518066de99138f3f3acf68f418eb350a
mandeeps708 <mandeeps708@gmail.com>
2015-11-02 02:02:04 +0530

    manual points

---
+++ dxf_demo.py
@@ -0,0 +1,29 @@
+from dxfwrite import DXFEngine as dxf
+import csv
+
+filename =raw_input('Enter a new name for the file:')
+drawing = dxf.drawing(filename+'.dxf')
+p8=(-0.8465,215.8)
+p9=(0.8465,215.8)
+drawing.add(dxf.line(p8, p9, color=7))
+
+p1=(-1.25,216.7)
+p2=(-1.05,217.05)
+p3=(-0.85,217.05)
+p4=(-0.65,216.4)
+p5=(0.65,216.4)
+p6=(0.85,217.05)
+p7=(1.05,217.05)
+p8=(1.25,216.7)
+
+drawing.add(dxf.line(p1, p2, color=7))
+drawing.add(dxf.line(p2, p3, color=7))
+drawing.add(dxf.line(p3, p4, color=7))
+drawing.add(dxf.line(p4, p5, color=7))
+drawing.add(dxf.line(p5, p6, color=7))
+drawing.add(dxf.line(p6, p7, color=7))
+drawing.add(dxf.line(p7, p8, color=7))
+drawing.add_layer('TEXTLAYER', color=2)
+drawing.add(dxf.text('Mandeep', insert=(0, 0.2), layer='TEXTLAYER'))
+drawing.save()
+print "Check file test.dxf in current directory."
